9c85f0846966abab659431c8f48a56ca3599d387
Yoel Zeitoun <yoelzeit@gmail.com>
2020-01-09 01:39:38 +0200

    ok

--- PL/HostInterface.xaml.cs
+++ PL/HostInterface.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,19 @@ namespace PL
         public HostInterface()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //OpenFileDialog op = new OpenFileDialog();
+            //op.Title = "Select a picture";
+            //op.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+            //  "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+            //  "Portable Network Graphic (*.png)|*.png";
+            //if (op.ShowDialog() == true)
+            //{
+            //    myPicture.Source = new BitmapImage(new Uri(op.FileName));
+            //}
         }
     }
 }
